63fd74d54bd18cb199293045137e51c2e6962af4
Joseba kerejeta <josebaquerejetaweb@gmail.com>
2024-10-16 17:51:56 +0200

    characters list changed

--- src/app/bdz/services/dbz.service.ts
+++ src/app/bdz/services/dbz.service.ts
@@ -5,22 +5,22 @@ import { v4 as uuid } from 'uuid';
 @Injectable({ providedIn: 'root' })
 export class DbzService {
   public characters: CharacterEntity[] = [
-    { name: "Goku", power: 1500000000 },
-    { name: "Vegeta", power: 1400000000 },
-    { name: "Gohan", power: 1200000000 },
-    { name: "Piccolo", power: 1000000000 },
-    { name: "Frieza", power: 1300000000 },
-    { name: "Cell", power: 1400000000 },
-    { name: "Majin Buu", power: 2000000000 },
-    { name: "Trunks", power: 1100000000 },
-    { name: "Krillin", power: 10000000 },
-    { name: "Tien", power: 7500000 },
-    { name: "Yamcha", power: 5000000 },
-    { name: "Android 18", power: 900000000 },
-    { name: "Android 17", power: 900000000 },
-    { name: "Broly", power: 2100000000 },
-    { name: "Beerus", power: 3000000000 },
-    { name: "Zamasu", power: 2500000000 },
+    { id: uuid(), name: "Goku", power: 1500000000 },
+    { id: uuid(), name: "Vegeta", power: 1400000000 },
+    { id: uuid(), name: "Gohan", power: 1200000000 },
+    { id: uuid(), name: "Piccolo", power: 1000000000 },
+    { id: uuid(), name: "Frieza", power: 1300000000 },
+    { id: uuid(), name: "Cell", power: 1400000000 },
+    { id: uuid(), name: "Majin Buu", power: 2000000000 },
+    { id: uuid(), name: "Trunks", power: 1100000000 },
+    { id: uuid(), name: "Krillin", power: 10000000 },
+    { id: uuid(), name: "Tien", power: 7500000 },
+    { id: uuid(), name: "Yamcha", power: 5000000 },
+    { id: uuid(), name: "Android 18", power: 900000000 },
+    { id: uuid(), name: "Android 17", power: 900000000 },
+    { id: uuid(), name: "Broly", power: 2100000000 },
+    { id: uuid(), name: "Beerus", power: 3000000000 },
+    { id: uuid(), name: "Zamasu", power: 2500000000 },
   ];
 
 
